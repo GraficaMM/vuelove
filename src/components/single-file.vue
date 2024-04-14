28d6958be846bc91fd30c12793108c69ccae784b
GraficaMM <grafica.multimedia.2023@gmail.com>
2024-04-14 17:57:14 +0200

    componente single-file

---
+++ src/components/single-file.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const saludo = ref('hola a todos')
+</script>
+
+<template>
+  <div>
+    {{ saludo }}
+  </div>
+</template>
+
+<style scoped></style>
